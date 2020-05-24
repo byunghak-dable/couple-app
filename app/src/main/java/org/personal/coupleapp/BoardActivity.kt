22a74babda4b3d28420db2841269fe9f1b2cad5e
ByungHakNoh <nbh6579@naver.com>
2020-05-24 15:48:33 +0900

    [add] 프로젝트 추가(삭제된 레포에 있던 파일들 한번에 추가)

---
+++ app/src/main/java/org/personal/coupleapp/BoardActivity.kt
@@ -0,0 +1,12 @@
+package org.personal.coupleapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class BoardActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_board)
+    }
+}
---
+++ app/src/main/java/org/personal/coupleapp/MainActivity.kt
@@ -0,0 +1,31 @@
+package org.personal.coupleapp
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.view.animation.AnimationUtils
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    private val SPLASH_SCREEN_DELAY_TIME: Long = 1500
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        splashAnimation()
+    }
+
+    private fun splashAnimation() {
+        val bottomAnimation = AnimationUtils.loadAnimation(this, R.anim.splash_from_bottom)
+        logoImage.animation = bottomAnimation
+
+        Handler().postDelayed({
+            val toSignIn = Intent(this, SignInActivity::class.java)
+            startActivity(toSignIn)
+            finish()
+        }, SPLASH_SCREEN_DELAY_TIME)
+    }
+}
