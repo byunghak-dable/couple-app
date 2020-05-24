7c06345808e7ce658dfa312d059d00b74d98456f
ByungHakNoh <nbh6579@naver.com>
2020-05-24 17:47:55 +0900

    [add] 회원가입 step.2 레이아웃 추가

---
+++ app/src/main/java/org/personal/coupleapp/SignUpFirstActivity.kt
@@ -0,0 +1,41 @@
+package org.personal.coupleapp
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_sign_up_first_step.*
+
+class SignUpFirstStepActivity : AppCompatActivity(), View.OnClickListener {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_up_first_step)
+
+        setListener()
+    }
+
+    private fun setListener() {
+        signUpBtn.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View?) {
+        when(v?.id) {
+            R.id.signUpBtn -> toSecondStep()
+            R.id.googleSignUpBtn -> googleSignUp()
+        }
+    }
+
+    //------------------ 클릭 시 이벤트 관리하는 메소드 모음 ------------------
+    // TODO: 데이터 이동 및 서버와의 통신 구현해야함
+
+    // 회원가입 두번 째 단계로 가는 메소드
+    private fun toSecondStep() {
+        val toSecondStep = Intent(this, SignUpSecondActivity::class.java)
+        startActivity(toSecondStep)
+    }
+
+    private fun googleSignUp() {
+
+    }
+}
