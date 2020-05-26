2003dc48aec1d10f5fac265526c8b1c4689415cf
ByungHakNoh <nbh6579@naver.com>
2020-05-26 22:52:03 +0900

    [add] http 서버와 통신하는 HandlerThread 구현완료

--- app/src/main/java/org/personal/coupleapp/backgroundOperation/ServerConnectionThread.kt
+++ app/src/main/java/org/personal/coupleapp/backgroundOperation/ServerConnectionThread.kt
@@ -6,13 +6,16 @@ import android.os.HandlerThread
 import android.os.Message
 import org.personal.coupleapp.utils.serverConnection.HTTPRequest
 
-class ServerConnectionThread(name: String?, priority: Int) : HandlerThread(name, priority) {
+class ServerConnectionThread(name: String?, private val mainHandler: Handler) : HandlerThread(name) {
 
     private val TAG = javaClass.name
 
     // msg.what 의 value 값 선언
-    private val FETCH_DATA = 1
-    private val POST_DATA = 2
+    companion object {
+        const val FETCH_DATA = 1
+        const val POST_DATA = 2
+    }
+
     private lateinit var handler: Handler
 
     @SuppressLint("HandlerLeak")
@@ -21,28 +24,36 @@ class ServerConnectionThread(name: String?, priority: Int) : HandlerThread(name,
             override fun handleMessage(msg: Message) {
                 when (msg.what) {
 
-                    FETCH_DATA -> fetchDataFromServer(msg.obj.toString())
+                    // 서버로부터 데이터를 받아오고 결과를 UIHandler 로 전송
+                    FETCH_DATA -> {
+                        val serverPage = msg.obj.toString()
+                        // mainHandler 의 msg.what 값은 msg.arg1 에서 추출
+                        val whichMessage = msg.arg1
+                        val httpRequest = HTTPRequest(serverPage)
+                        val message = Message.obtain(mainHandler)
+
+                        message.obj = httpRequest.fetchFromServer()
+                        message.what = whichMessage
+                        message.sendToTarget()
+                    }
+
+                    // 서버에 데이터를 전송하고 서버에서 받은 결과를 UIHandler 로 전송
                     POST_DATA -> {
                         // HashMap 으로 보낸 msg.obj 객체 캐스팅
                         val msgObjHashMap = msg.obj as HashMap<*, *>
+                        // mainHandler 의 msg.what 값은 msg.arg1 에서 추출
+                        val whichMessage = msg.arg1
                         val serverPage = msgObjHashMap["serverPage"].toString()
-                        val postJasonString = msgObjHashMap["postJasonString"].toString()
-                        postDataToServer(serverPage, postJasonString)
+                        val postJasonString = msgObjHashMap["postJsonString"].toString()
+
+                        val httpRequest = HTTPRequest(serverPage)
+                        val message = Message.obtain(mainHandler)
+
+                        message.obj = httpRequest.postToServer(postJasonString)
+                        message.what = whichMessage
+                        message.sendToTarget()
                     }
-               }
-            }
-            // TODO: 서버로 부터 받아온 데이터 handle 해야함
-
-            // 서버로부터 데이터를 받아오는 메소드
-            private fun fetchDataFromServer(serverPage: String) {
-                val httpRequest = HTTPRequest(serverPage)
-                httpRequest.fetchFromServer()
-            }
-
-            // 서버에 데이터를 전송하는 메소드
-            private fun postDataToServer(serverPage: String, postJasonString: String) {
-                val httpRequest = HTTPRequest(serverPage)
-                httpRequest.postToServer(postJasonString)
+                }
             }
         }
     }
