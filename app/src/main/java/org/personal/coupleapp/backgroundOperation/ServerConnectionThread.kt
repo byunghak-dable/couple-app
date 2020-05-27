1daeefb0419437bf9228347d93c4c582af58f422
ByungHakNoh <nbh6579@naver.com>
2020-05-27 20:31:05 +0900

    [add] msg.arg2 값에 따라 HTTPRequest 클래스의 따라 호출 메소드 정함

--- app/src/main/java/org/personal/coupleapp/backgroundOperation/ServerConnectionThread.kt
+++ app/src/main/java/org/personal/coupleapp/backgroundOperation/ServerConnectionThread.kt
@@ -8,13 +8,14 @@ import org.personal.coupleapp.utils.serverConnection.HTTPRequest
 
 class ServerConnectionThread(name: String?, private val mainHandler: Handler) : HandlerThread(name) {
 
-    private val TAG = javaClass.name
-
     // msg.what 의 value 값 선언
     companion object {
         const val FETCH_DATA = 1
         const val POST_DATA = 2
+        const val REQUEST_POSTING = 1
     }
+
+    private val TAG = javaClass.name
 
     private lateinit var handler: Handler
 
@@ -22,13 +23,14 @@ class ServerConnectionThread(name: String?, private val mainHandler: Handler) : 
     override fun onLooperPrepared() {
         handler = object : Handler() {
             override fun handleMessage(msg: Message) {
+                // mainHandler 의 msg.what 값은 msg.arg1 에서 추출
+                val whichMessage = msg.arg1
+
                 when (msg.what) {
 
                     // 서버로부터 데이터를 받아오고 결과를 UIHandler 로 전송
                     FETCH_DATA -> {
                         val serverPage = msg.obj.toString()
-                        // mainHandler 의 msg.what 값은 msg.arg1 에서 추출
-                        val whichMessage = msg.arg1
                         val httpRequest = HTTPRequest(serverPage)
                         val message = Message.obtain(mainHandler)
 
@@ -39,17 +41,20 @@ class ServerConnectionThread(name: String?, private val mainHandler: Handler) : 
 
                     // 서버에 데이터를 전송하고 서버에서 받은 결과를 UIHandler 로 전송
                     POST_DATA -> {
+                        // 어떤 HTTPRequest 메소드를 사용할 지 정함
+                        val whichRequest = msg.arg2
                         // HashMap 으로 보낸 msg.obj 객체 캐스팅
                         val msgObjHashMap = msg.obj as HashMap<*, *>
-                        // mainHandler 의 msg.what 값은 msg.arg1 에서 추출
-                        val whichMessage = msg.arg1
                         val serverPage = msgObjHashMap["serverPage"].toString()
                         val postJasonString = msgObjHashMap["postJsonString"].toString()
 
                         val httpRequest = HTTPRequest(serverPage)
                         val message = Message.obtain(mainHandler)
 
-                        message.obj = httpRequest.postToServer(postJasonString)
+                        when (whichRequest) {
+                            REQUEST_POSTING -> message.obj = httpRequest.postToServer(postJasonString)
+                        }
+
                         message.what = whichMessage
                         message.sendToTarget()
                     }
