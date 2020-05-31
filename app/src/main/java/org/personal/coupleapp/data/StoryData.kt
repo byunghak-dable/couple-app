6a1f755f4470b62a12b15ec6d14c277a745d0e2c
ByungHakNoh <nbh6579@naver.com>
2020-05-31 16:10:44 +0900

    [add] 어뎁터 및 데이터 추가 테스트

---
+++ app/src/main/java/org/personal/coupleapp/data/StoryData.kt
@@ -0,0 +1,6 @@
+package org.personal.coupleapp.data
+
+import com.denzcoskun.imageslider.models.SlideModel
+
+//val profileImage: String, val title: String, 사이에 , val description: String
+data class StoryData(val imageList: ArrayList<SlideModel>)
