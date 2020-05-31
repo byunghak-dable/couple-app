dd2871d9cb054170f5545d07ed490a0424a875a6
ByungHakNoh <nbh6579@naver.com>
2020-05-31 18:41:08 +0900

    [add] 프로필 수정 액티비티 추가

--- app/src/main/java/org/personal/coupleapp/ProfileModifyActivity.kt
+++ app/src/main/java/org/personal/coupleapp/ProfileModifyActivity.kt
@@ -7,6 +7,6 @@ class ProfileActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_profile2)
+        setContentView(R.layout.activity_profile)
     }
 }
