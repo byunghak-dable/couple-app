9ed9f657b383a6b283b4a364411a81475b991b13
ByungHakNoh <nbh6579@naver.com>
2020-06-01 11:15:16 +0900

    [add] 프로필 정보 서버로 전송하는 메소드 추가

--- app/src/main/java/org/personal/coupleapp/utils/serverConnection/HTTPOutPut.kt
+++ app/src/main/java/org/personal/coupleapp/utils/serverConnection/HTTPOutPut.kt
@@ -1,11 +1,13 @@
 package org.personal.coupleapp.utils.serverConnection
 
-import org.personal.coupleapp.data.UserData
+import org.personal.coupleapp.data.ProfileData
+import org.personal.coupleapp.data.SingleUserData
 
 // HTTPRequest 에 implement, 서버와의 통신 후 결과를 반환하는 메소드 포함
 interface HTTPOutPut {
 
     // TODO: 각각의 data 리스트를 반환할 지 jsonString 을 반환할 지 정해야 함.
-    fun fetchFromServer(): List<UserData>
+    fun fetchFromServer(): List<SingleUserData>
     fun postToServer(postJsonString: String): String
+    fun postProfileToServer(profileData: ProfileData): String
 }
