65048f4442e74818d73a9a1089a78bb43660aaaa
ByungHakNoh <nbh6579@naver.com>
2020-06-01 14:40:55 +0900

    [add] 로딩 다이얼로그 추가

---
+++ app/src/main/java/org/personal/coupleapp/dialog/LoadingDialog.kt
@@ -0,0 +1,25 @@
+package org.personal.coupleapp.dialog
+
+import android.app.AlertDialog
+import android.app.Dialog
+import android.os.Bundle
+import androidx.fragment.app.DialogFragment
+import org.personal.coupleapp.R
+
+class LoadingDialog : DialogFragment() {
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        val inflater = requireActivity().layoutInflater
+        val dialogView = inflater.inflate(R.layout.dialog_loading, null)
+
+        return AlertDialog.Builder(activity)
+            .setView(dialogView)
+            .create()
+
+    }
+
+    override fun dismiss() {
+        if (dialog != null && dialog!!.isShowing) {
+            dialog!!.dismiss()
+        }
+    }
+}
