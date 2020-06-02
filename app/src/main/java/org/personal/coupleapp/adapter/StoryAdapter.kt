4751e222e87ef6085432baf4f1a0a6bdf9fe5c99
ByungHakNoh <nbh6579@naver.com>
2020-06-02 23:35:34 +0900

    [add] 스토리 어뎁터 뷰들 데이터 넣어서 보여주기

--- app/src/main/java/org/personal/coupleapp/adapter/StoryAdapter.kt
+++ app/src/main/java/org/personal/coupleapp/adapter/StoryAdapter.kt
@@ -1,20 +1,28 @@
 package org.personal.coupleapp.adapter
 
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.denzcoskun.imageslider.ImageSlider
 import com.denzcoskun.imageslider.models.SlideModel
 import org.personal.coupleapp.R
 import org.personal.coupleapp.data.StoryData
+import org.personal.coupleapp.utils.singleton.CalendarHelper
 
 class StoryAdapter(private val storyList: ArrayList<StoryData>, private val itemClickListener: ItemClickListener) :
     RecyclerView.Adapter<StoryAdapter.ViewHolder>() {
 
+    val VIEW_TYPE_LOADING = 1
+    val VIEW_TYPE_ITEM = 2
 
     class ViewHolder(itemView: View, val itemClickListener: ItemClickListener) : RecyclerView.ViewHolder(itemView) {
         val imageSlider: ImageSlider = itemView.findViewById(R.id.imageSliderIS)
+        val titleTV : TextView = itemView.findViewById(R.id.titleTV)
+        val timeTV : TextView = itemView.findViewById(R.id.timeTV)
+        val descriptionTV : TextView = itemView.findViewById(R.id.descriptionTV)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): StoryAdapter.ViewHolder {
@@ -26,8 +34,24 @@ class StoryAdapter(private val storyList: ArrayList<StoryData>, private val item
         return storyList.size
     }
 
+
+    override fun getItemViewType(position: Int): Int {
+
+        return if (storyList[position] == null) VIEW_TYPE_LOADING else VIEW_TYPE_ITEM
+    }
+
     override fun onBindViewHolder(holder: StoryAdapter.ViewHolder, position: Int) {
         val storyData: StoryData = storyList[position]
-        holder.imageSlider.setImageList(storyData.photo_path as ArrayList<SlideModel>, true)
+        val slideModelList = ArrayList<SlideModel>()
+
+        storyData.photo_path.forEach{
+            val slideModel = SlideModel(it as String)
+            slideModelList.add(slideModel)
+        }
+
+        holder.imageSlider.setImageList(slideModelList, true)
+        holder.titleTV.text = storyData.title
+        holder.timeTV.text = CalendarHelper.timeInMillsToDate(storyData.date)
+        holder.descriptionTV.text = storyData.description
     }
 }
