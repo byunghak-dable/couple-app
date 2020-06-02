81c258f31ac1318364dc8ed8ac8acffe950fd5d9
ByungHakNoh <nbh6579@naver.com>
2020-06-02 15:18:11 +0900

    [add] 매니페스트 액티비티 추가

---
+++ app/src/main/java/org/personal/coupleapp/StoryAddActivity.kt
@@ -0,0 +1,11 @@
+package org.personal.coupleapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class StoryAddActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_story_add)
+    }
+}
