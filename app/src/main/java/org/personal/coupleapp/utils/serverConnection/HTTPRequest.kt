19ef739c20c4d91d1448e75e83daf8d966aa6f67
ByungHakNoh <nbh6579@naver.com>
2020-06-03 14:37:19 +0900

    [modify] get, post, put, delete 메소드 명확하게 분류

--- app/src/main/java/org/personal/coupleapp/utils/serverConnection/HTTPRequest.kt
+++ app/src/main/java/org/personal/coupleapp/utils/serverConnection/HTTPRequest.kt
@@ -3,9 +3,9 @@ package org.personal.coupleapp.utils.serverConnection
 import android.graphics.Bitmap
 import android.util.Log
 import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
 import org.json.JSONObject
 import org.personal.coupleapp.data.ProfileData
-import org.personal.coupleapp.data.SingleUserData
 import org.personal.coupleapp.data.StoryData
 import org.personal.coupleapp.utils.singleton.ImageEncodeHelper
 
@@ -20,53 +20,44 @@ class HTTPRequest(private val serverPage: String) : HTTPOutPut {
     // domain 과 페이지를 통해 url 완성
     private val hTTPConnection: HTTPConnection = HTTPConnection(serverDomain + serverPage)
 
-    // TODO : 실제 데이터 handle 해야 함
-    override fun fetchFromServer(): List<SingleUserData> {
-        val returnDataList = ArrayList<SingleUserData>()
+    //------------------ GET 관련 메소드 모음 ------------------
+    override fun getMethodToServer(): String {
         val jsonString: String = hTTPConnection.getRequest()
-
-        return returnDataList
-    }
-
-    // 서버에 posting 을 하거나 하나의 value 만을 받을 때 사용
-    override fun postToServer(postJsonString: String): String {
-        val jsonString = hTTPConnection.postRequest(postJsonString)
 
         return jsonString.replace("\"", "")
     }
 
-    // 프로파일 정보를 서버로 보내는 메소드
-    override fun postProfileToServer(profileData: ProfileData): String {
-        val jsonString: String
+    override fun getProfileFromServer(): ProfileData {
         val gson = Gson()
-        val postJson: String
-        // 비트맵 이미지를 base64 로 변환
-        val base64Image = ImageEncodeHelper.bitmapToString(profileData.profile_image as Bitmap)
-
-        // 변환한 이미지를 다시 선언하여 Bitmap 에서 String 타입이 변환
-        profileData.profile_image = base64Image
-        postJson = gson.toJson(profileData)
-
-        // 결과를 받는다(성공 여부)
-        jsonString = hTTPConnection.postRequest(postJson)
-        Log.i(TAG, jsonString)
-        return jsonString
-    }
-
-    override fun getProfileFromServer(singleUserID: Int): ProfileData {
-        val gson = Gson()
-        val jsonObject = JSONObject()
-
-        jsonObject.put("id", singleUserID)
-
-        val jsonString: String = hTTPConnection.postRequest(jsonObject.toString())
+        val jsonString: String = hTTPConnection.getRequest()
         val profileData = gson.fromJson(jsonString, ProfileData::class.java)
         val profileImageBitmap = ImageEncodeHelper.getServerImage(profileData.profile_image.toString())
         profileData.profile_image = profileImageBitmap
         return profileData
     }
 
-    override fun postStoryToServer(storyData: StoryData): String {
+    override fun getStoryFromServer(): ArrayList<StoryData>? {
+        val gson = Gson()
+
+        // 결과를 받는다(성공 여부)
+        val jsonString: String = hTTPConnection.getRequest()
+
+        return if (jsonString != "false") {
+            gson.fromJson(jsonString, object : TypeToken<ArrayList<StoryData>>() {}.type)
+        } else {
+            null
+        }
+    }
+
+    //------------------ POST 관련 메소드 모음 ------------------
+    // 서버에 posting 을 하거나 하나의 value 만을 받을 때 사용
+    override fun postMethodToServer(postJsonString: String): String {
+        val jsonString = hTTPConnection.postRequest(postJsonString)
+
+        return jsonString.replace("\"", "")
+    }
+
+    override fun postStoryToServer(storyData: StoryData, what: String): String {
         val jsonString: String
         val gson = Gson()
         val postJson: String
@@ -81,9 +72,46 @@ class HTTPRequest(private val serverPage: String) : HTTPOutPut {
         storyData.photo_path = base64ImageList as ArrayList<Any>
         postJson = gson.toJson(storyData)
 
+        // gson 으로 변환한 json 에 어떤 작업을 하는지 알려주는 what 요소 추가
+        val jsonObject = JSONObject(postJson)
+        jsonObject.put("what", what)
+
         // 결과를 받는다(성공 여부)
-        jsonString = hTTPConnection.postRequest(postJson)
+        jsonString = hTTPConnection.postRequest(jsonObject.toString())
         Log.i(TAG, jsonString)
         return jsonString
+    }
+
+    //------------------ PUT 관련 메소드 모음 ------------------
+
+    override fun putMethodToServer(postJsonString: String): String {
+        val jsonString = hTTPConnection.putRequest(postJsonString)
+        Log.i(TAG, "put, delete 체크 : $jsonString ")
+        return jsonString.replace("\"", "")
+    }
+
+    // 프로파일 정보를 서버로 보내는 메소드
+    override fun putProfileToServer(profileData: ProfileData): String {
+        val jsonString: String
+        val gson = Gson()
+        val postJson: String
+        // 비트맵 이미지를 base64 로 변환
+        val base64Image = ImageEncodeHelper.bitmapToString(profileData.profile_image as Bitmap)
+
+        // 변환한 이미지를 다시 선언하여 Bitmap 에서 String 타입이 변환
+        profileData.profile_image = base64Image
+        postJson = gson.toJson(profileData)
+
+        // 결과를 받는다(성공 여부)
+        jsonString = hTTPConnection.putRequest(postJson)
+        Log.i(TAG, jsonString)
+        return jsonString
+    }
+
+    //------------------ DELETE 관련 메소드 모음 ------------------
+    // 삭제하는 메소드
+    override fun deleteMethodToServer(postJsonString: String): String {
+        val jsonString = hTTPConnection.deleteRequest(postJsonString)
+        return jsonString.replace("\"", "")
     }
 }
