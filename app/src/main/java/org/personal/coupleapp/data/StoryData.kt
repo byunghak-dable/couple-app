43a8f5598837c901c0669dc9fb565a21ef1cfe8d
ByungHakNoh <nbh6579@naver.com>
2020-06-03 14:25:11 +0900

    [add] parcelable 추가

--- app/src/main/java/org/personal/coupleapp/data/StoryData.kt
+++ app/src/main/java/org/personal/coupleapp/data/StoryData.kt
@@ -1,3 +1,40 @@
 package org.personal.coupleapp.data
 
-data class StoryData(val id: Int?, val couple_id: Int, val title: String, val description: String, val date: Int, var photo_path: ArrayList<Any>)
+import android.os.Parcel
+import android.os.Parcelable
+
+@Suppress("UNREACHABLE_CODE")
+data class StoryData(val id: Int?, val couple_id: Int, val title: String, val description: String, val date: Int, var photo_path: ArrayList<Any>) : Parcelable {
+    constructor(parcel: Parcel) : this(
+        parcel.readValue(Int::class.java.classLoader) as? Int,
+        parcel.readInt(),
+        parcel.readString()!!,
+        parcel.readString()!!,
+        parcel.readInt(),
+        parcel.readArrayList(ArrayList::class.java.classLoader) as ArrayList<Any>
+    )
+
+    override fun writeToParcel(parcel: Parcel, flags: Int) {
+        parcel.writeValue(id)
+        parcel.writeInt(couple_id)
+        parcel.writeString(title)
+        parcel.writeString(description)
+        parcel.writeInt(date)
+        parcel.writeList(photo_path as List<*>)
+    }
+
+    override fun describeContents(): Int {
+        return 0
+    }
+
+    companion object CREATOR : Parcelable.Creator<StoryData> {
+        override fun createFromParcel(parcel: Parcel): StoryData {
+            return StoryData(parcel)
+        }
+
+        override fun newArray(size: Int): Array<StoryData?> {
+            return arrayOfNulls(size)
+        }
+    }
+
+}
