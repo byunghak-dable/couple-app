59e8954d85b9c6dfe0c11486d5edf963f4d1be6a
ByungHakNoh <nbh6579@naver.com>
2020-06-04 14:41:50 +0900

    [add] 캘린더 꾸미는 클래스 추가

---
+++ app/src/main/java/org/personal/coupleapp/utils/calendar/OneDayDecorator.kt
@@ -0,0 +1,39 @@
+package org.personal.coupleapp.utils.calendar
+
+import android.graphics.Color
+import android.graphics.Typeface
+import android.text.style.ForegroundColorSpan
+import android.text.style.RelativeSizeSpan
+import android.text.style.StyleSpan
+import com.prolificinteractive.materialcalendarview.CalendarDay
+import com.prolificinteractive.materialcalendarview.DayViewDecorator
+import com.prolificinteractive.materialcalendarview.DayViewFacade
+import com.prolificinteractive.materialcalendarview.MaterialCalendarView
+import java.util.*
+
+
+class OneDayDecorator : DayViewDecorator {
+
+    private var date: CalendarDay
+
+    init {
+        date = CalendarDay.today()
+    }
+
+    override fun shouldDecorate(day: CalendarDay): Boolean {
+        return day == date
+    }
+
+    override fun decorate(view: DayViewFacade) {
+        view.addSpan(StyleSpan(Typeface.BOLD))
+        view.addSpan(RelativeSizeSpan(1.4f))
+        view.addSpan(ForegroundColorSpan(Color.GREEN))
+    }
+
+    /**
+     * We're changing the internals, so make sure to call [MaterialCalendarView.invalidateDecorators]
+     */
+    fun setDate(date: Date?) {
+        this.date = CalendarDay.from(date)
+    }
+}
