779502ff69b479101a5c2aa7c22aa21389a8303c
ByungHakNoh <nbh6579@naver.com>
2020-06-04 14:43:00 +0900

    [modify] 캘린더 객체 전역 변수에서 지역 변수로 수정

--- app/src/main/java/org/personal/coupleapp/utils/singleton/CalendarHelper.kt
+++ app/src/main/java/org/personal/coupleapp/utils/singleton/CalendarHelper.kt
@@ -6,21 +6,22 @@ import kotlin.collections.ArrayList
 
 object CalendarHelper {
 
-    private val calendar = Calendar.getInstance()
 
-    val timeList:ArrayList<Int> = ArrayList()
+    val timeList: ArrayList<Long> = ArrayList()
 
     // 시간과 분을 입력 받고 사용자가 보기 편하게 시간 텍스트로 보여주는 메소드
     fun setTimeFormat(hour: Int, minute: Int): String {
+        val calendar = Calendar.getInstance()
         calendar[Calendar.HOUR_OF_DAY] = hour
         calendar[Calendar.MINUTE] = minute
         calendar[Calendar.SECOND] = 0
 
-        return DateFormat.getTimeInstance().format(calendar.time)
+        return DateFormat.getTimeInstance(DateFormat.SHORT).format(calendar.time)
     }
 
     // 년도, 월, 일을 입력 받고 Date 을 사용자에게 텍스트로 보여주는 메소드
     fun setDateFormat(year: Int, month: Int, dayOfMonth: Int): String {
+        val calendar = Calendar.getInstance()
         calendar[Calendar.YEAR] = year
         calendar[Calendar.MONTH] = month
         calendar[Calendar.DAY_OF_MONTH] = dayOfMonth
@@ -29,39 +30,53 @@ object CalendarHelper {
     }
 
     // 날짜를 밀리세컨으로 변환하는 메소드
-    fun dateToTimeInMills(year: Int, month: Int, dayOfMonth: Int): Int {
+    fun dateToTimeInMills(year: Int, month: Int, dayOfMonth: Int): Long {
+        val calendar = Calendar.getInstance()
         calendar[Calendar.YEAR] = year
         calendar[Calendar.MONTH] = month
         calendar[Calendar.DAY_OF_MONTH] = dayOfMonth
 
-        return calendar.timeInMillis.toInt()
+        return calendar.timeInMillis
     }
 
-    fun timeInMillsToDate(timeInMills: Int): String {
-        calendar.timeInMillis =  timeInMills.toLong()
+    fun timeInMillsToDate(timeInMills: Long): String {
+        val calendar = Calendar.getInstance()
+        calendar.timeInMillis = timeInMills
 
         return DateFormat.getDateInstance().format(calendar.time)
+    }
+
+    fun timeInMillsToTime(timeInMills: Long): String {
+        val calendar = Calendar.getInstance()
+        calendar.timeInMillis = timeInMills
+
+        return DateFormat.getTimeInstance(DateFormat.SHORT).format(calendar.time)
     }
 
     //------------------ 현재 날짜를 가져오는 메소드들 ------------------
 
     fun getCurrentHour(): Int {
+        val calendar = Calendar.getInstance()
         return calendar.get(Calendar.HOUR)
     }
 
     fun getCurrentMinute(): Int {
+        val calendar = Calendar.getInstance()
         return calendar.get(Calendar.MINUTE)
     }
 
     fun getCurrentYear(): Int {
+        val calendar = Calendar.getInstance()
         return calendar.get(Calendar.YEAR)
     }
 
     fun getCurrentMonth(): Int {
+        val calendar = Calendar.getInstance()
         return calendar.get(Calendar.MONTH)
     }
 
     fun getCurrentDay(): Int {
+        val calendar = Calendar.getInstance()
         return calendar.get(Calendar.DAY_OF_MONTH)
     }
 }
