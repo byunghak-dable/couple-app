48fa93bc5af77030dabd263aefbc81fb5c68462e
ByungHakNoh <nbh6579@naver.com>
2020-06-07 14:40:43 +0900

    [add] 두번 째 회원가입 http스레드 작업을 바인드 서비스에서 처리하도록 전환

--- app/src/main/java/org/personal/coupleapp/SignUpSecondActivity.kt
+++ app/src/main/java/org/personal/coupleapp/SignUpSecondActivity.kt
@@ -1,59 +1,52 @@
 package org.personal.coupleapp
 
+import android.content.ComponentName
 import android.content.Intent
+import android.content.ServiceConnection
 import android.net.Uri
+import android.os.*
 import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
-import android.os.Handler
-import android.os.Message
 import android.util.Log
 import android.view.View
-import android.widget.TextView
 import kotlinx.android.synthetic.main.activity_sign_up_second.*
 import org.json.JSONObject
-import org.personal.coupleapp.SignUpSecondActivity.CustomHandler.Companion.CHECK_CONNECTION
-import org.personal.coupleapp.SignUpSecondActivity.CustomHandler.Companion.GET_INVITE_CODE
-import org.personal.coupleapp.SignUpSecondActivity.CustomHandler.Companion.CHECK_CONNECTE_COMPLETED
-import org.personal.coupleapp.backgroundOperation.ServerConnectionThread
-import org.personal.coupleapp.backgroundOperation.ServerConnectionThread.Companion.REQUEST_SIMPLE_POST_METHOD
+import org.personal.coupleapp.backgroundOperation.HTTPConnectionThread.Companion.REQUEST_SIMPLE_POST_METHOD
 import org.personal.coupleapp.dialog.InformDialog
 import org.personal.coupleapp.dialog.LoadingDialog
-import org.personal.coupleapp.utils.singleton.HandlerMessageHelper
+import org.personal.coupleapp.service.HTTPConnectionInterface
+import org.personal.coupleapp.service.HTTPConnectionService
 import org.personal.coupleapp.utils.singleton.SharedPreferenceHelper
 import java.lang.Integer.parseInt
-import java.lang.ref.WeakReference
 
 class SignUpSecondActivity : AppCompatActivity(), View.OnClickListener {
 
     private val TAG = javaClass.name
     private val serverPage = "ConnectPartner"
-    private lateinit var serverConnectionThread: ServerConnectionThread
+
+    private lateinit var httpConnectionService: HTTPConnectionService
     private val loadingDialog by lazy { LoadingDialog() }
+
+    private val GET_INVITE_CODE = 1
+    private val CHECK_CONNECTE_COMPLETED = 2
+    private val CHECK_CONNECTION = 3
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         Log.i("thread-test", "onCreate")
         setContentView(R.layout.activity_sign_up_second)
         setListener()
-        startWorkerThread()
     }
 
     override fun onStart() {
         super.onStart()
         Log.i("thread-test", "onStart")
         getDeepLinkParameter()
+        startBoundService()
     }
 
-    override fun onResume() {
-        super.onResume()
-        Log.i("thread-test", "onResume")
-        getInviteCode()
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
-        Log.i("thread-test", "onDestroy")
-        stopWorkerThread()
+    override fun onStop() {
+        super.onStop()
+        unbindService(connection)
     }
 
     //------------------ 액티비티 생명주기 콜백 메소드에서 호출할 메소드 모음 ------------------
@@ -63,35 +56,6 @@ class SignUpSecondActivity : AppCompatActivity(), View.OnClickListener {
         checkConnectedBtn.setOnClickListener(this)
     }
 
-    // 백그라운드 스레드 실행
-    private fun startWorkerThread() {
-        val mainHandler = CustomHandler(this, myInviteCodeTV, loadingDialog)
-        serverConnectionThread = ServerConnectionThread("ServerConnectionHelper", mainHandler)
-        serverConnectionThread.start()
-    }
-
-    // 백그라운드의 루퍼를 멈춰줌으로써 스레드 종료
-    private fun stopWorkerThread() {
-        Log.i(TAG, "thread 종료")
-        serverConnectionThread.looper.quit()
-    }
-
-    private fun getInviteCode() {
-        // singleUserColumn id 를 preference 에서 추출해 서버로 전송
-        val postJsonObject = JSONObject()
-        val userColumnID = SharedPreferenceHelper.getInt(this, getText(R.string.userColumnID).toString())
-
-        postJsonObject.put("what", "getInvitationCode")
-        postJsonObject.put("singleUserID", userColumnID)
-
-        Handler().postDelayed({
-
-            HandlerMessageHelper.serverPostRequest(serverConnectionThread, serverPage, postJsonObject.toString(), GET_INVITE_CODE, REQUEST_SIMPLE_POST_METHOD)
-        }, 500)
-        Log.i(TAG, "초대코드 서버로부터 받아오는 요청 보냄")
-        Log.i("thread-test", "onResume 끝")
-    }
-
     private fun getDeepLinkParameter() {
         val uri: Uri? = intent.data
         if (uri != null) {
@@ -99,6 +63,12 @@ class SignUpSecondActivity : AppCompatActivity(), View.OnClickListener {
             val opponentCode = parameters[parameters.size - 1]
             opponentCodeED.setText(opponentCode)
         }
+    }
+
+    // 현재 액티비티와 HTTPConnectionService(Bound Service)를 연결하는 메소드
+    private fun startBoundService() {
+        val startService = Intent(this, HTTPConnectionService::class.java)
+        bindService(startService, connection, BIND_AUTO_CREATE)
     }
 
     //------------------ 클릭 시 이벤트 관리하는 메소드 모음 ------------------
@@ -128,7 +98,7 @@ class SignUpSecondActivity : AppCompatActivity(), View.OnClickListener {
         jsonObject.put("invitationReceiverID", invitationSenderID)
         jsonObject.put("invitationSenderCode", parseInt(opponentCodeED.text.toString()))
 
-        HandlerMessageHelper.serverPostRequest(serverConnectionThread, serverPage, jsonObject.toString(), CHECK_CONNECTE_COMPLETED, REQUEST_SIMPLE_POST_METHOD)
+        httpConnectionService.serverPostRequest(serverPage, jsonObject.toString(), REQUEST_SIMPLE_POST_METHOD, CHECK_CONNECTE_COMPLETED)
         Log.i(TAG, "커플 DB에 업로드")
     }
 
@@ -140,104 +110,104 @@ class SignUpSecondActivity : AppCompatActivity(), View.OnClickListener {
         jsonObject.put("what", "checkConnection")
         jsonObject.put("id", myColumnID)
 
-        HandlerMessageHelper.serverPostRequest(serverConnectionThread, serverPage, jsonObject.toString(), CHECK_CONNECTION, REQUEST_SIMPLE_POST_METHOD)
-        Log.i(TAG, "상대방과의 연결확인")
-        Log.i(TAG, jsonObject.toString())
+        httpConnectionService.serverPostRequest(serverPage, jsonObject.toString(), REQUEST_SIMPLE_POST_METHOD, CHECK_CONNECTION)
+
+        Log.i(TAG, "Request : 상대방과의 연결확인하기")
     }
 
-    // 초대 코드를 받아오고,
-    private class CustomHandler(activity: AppCompatActivity, myInviteCode: TextView, loadingDialog: LoadingDialog) : Handler() {
-
-        companion object {
-            const val GET_INVITE_CODE = 1
-            const val CHECK_CONNECTE_COMPLETED = 2
-            const val CHECK_CONNECTION = 3
+    // Memo : BoundService 의 IBinder 객체를 받아와 현재 액티비티에서 서비스의 메소드를 사용하기 위한 클래스
+    /*
+    바운드 서비스에서는 HTTPConnectionThread(HandlerThread)가 동작하고 있으며, 이 스레드에 메시지를 통해 서버에 요청을 보낸다
+    서버에서 결과를 보내주면 HTTPConnectionThread(HandlerThread)의 인터페이스 메소드 -> 바운드 서비스 -> 바운드 서비스 인터페이스 -> 액티비티 onHttpRespond 에서 handle 한다
+     */
+    private val connection: ServiceConnection = object : ServiceConnection, HTTPConnectionInterface {
+        override fun onServiceConnected(name: ComponentName, service: IBinder) {
+            val binder: HTTPConnectionService.LocalBinder = service as HTTPConnectionService.LocalBinder
+            httpConnectionService = binder.getService()!!
+            httpConnectionService.setOnHttpRespondListener(this)
+            getInviteCode()
         }
 
-        private val TAG = javaClass.name
+        override fun onServiceDisconnected(name: ComponentName) {
+            Log.i(TAG, "바운드 서비스 연결 종료")
+        }
 
-        private val activityWeak: WeakReference<AppCompatActivity> = WeakReference(activity)
-        private val myInviteCodeWeak: WeakReference<TextView> = WeakReference(myInviteCode)
-        private val loadingDialog: WeakReference<LoadingDialog> = WeakReference(loadingDialog)
+        override fun onHttpRespond(responseData: HashMap<*, *>) {
+            // 메인 UI 작업을 담당하는 핸들러 -> 액티비티가 아닌 ServiceConnection 의 추상 클래스 내부에서 UI 를 다루기 위해 생성
+            val handler = Handler(Looper.getMainLooper())
 
-        override fun handleMessage(msg: Message) {
-            super.handleMessage(msg)
+            when (responseData["whichRespond"] as Int) {
 
-            val activity = activityWeak.get()
-            val myInviteCode = myInviteCodeWeak.get()
+                GET_INVITE_CODE -> {
+                    val invitationCode = responseData["respondData"].toString()
+                    handler.post { myInviteCodeTV.text = invitationCode }
+                    Log.i(TAG, "onResponseData : ${responseData["respondData"]}")
+                }
+                CHECK_CONNECTE_COMPLETED -> {
+                    loadingDialog.dismiss()
 
-            // 액티비티가 destroy 되지 않았을 때
-            if (activity != null) {
-                when (msg.what) {
-                    // 초대 코드 받기
-                    GET_INVITE_CODE -> {
-                        val invitationCode = msg.obj.toString()
-                        myInviteCode?.text = invitationCode
-                    }
-                    // 연결 완료
-                    CHECK_CONNECTE_COMPLETED -> {
-                        // 로딩 다이얼로그 없애기
-                        loadingDialog.get()?.dismiss()
-                        when (msg.obj.toString()) {
-                            "false" -> {
-                                val alertDialog = InformDialog()
-                                val arguments = Bundle()
-
-                                arguments.putString("title", "알림창")
-                                arguments.putString("message", "상대방 코드를 확인해주세요")
-
-                                alertDialog.arguments = arguments
-                                alertDialog.show(activity.supportFragmentManager, "WrongOpponentCode")
-                            }
-
-                            else -> {
-                                onSignUpSuccess(activity, msg)
-
-                            }
+                    when (responseData["respondData"].toString()) {
+                        "false" -> {
+                            failedDialog("상대방 코드를 확인해주세요")
                         }
-                    }
-
-                    // 연결 확인 하기
-                    CHECK_CONNECTION -> {
-                        // 로딩 다이얼로그 숨기기
-                        loadingDialog.get()?.dismiss()
-                        when (msg.obj.toString()) {
-                            "false" -> {
-                                val alertDialog = InformDialog()
-                                val arguments = Bundle()
-
-                                arguments.putString("title", "알림창")
-                                arguments.putString("message", "상대방과 연결이 되지 않았습니다")
-
-                                alertDialog.arguments = arguments
-                                alertDialog.show(activity.supportFragmentManager, "WrongOpponentCode")
-                            }
-
-                            else -> {
-                                onSignUpSuccess(activity, msg)
-                            }
+                        else -> {
+                            val coupleColumnID = parseInt(responseData["respondData"].toString())
+                            onSignUpSuccess(this@SignUpSecondActivity, coupleColumnID)
                         }
                     }
                 }
-                // 액티비티가 destroy 되면 바로 빠져나오도록
-            } else {
-                Log.i(TAG, "Handler 멈춤")
-                return
+                CHECK_CONNECTION -> {
+                    when (responseData["respondData"].toString()) {
+                        "false" -> {
+                            failedDialog("상대방과 연결이 되지 않았습니다")
+                        }
+                        else -> {
+                            val coupleColumnID = parseInt(responseData["respondData"].toString())
+                            onSignUpSuccess(this@SignUpSecondActivity, coupleColumnID)
+                        }
+                    }
+                }
+
             }
         }
 
-        // 상대방과 연결이 완료 되었을 때(회원 가입 2단계까지 완료) -> preference 에 커플 컬럼 id, 커플 연결 완료 Boolean 값(true) 저장
-        private fun onSignUpSuccess(activity: AppCompatActivity, msg: Message) {
+        // 회원가입 2단계에 접속하면 1단계 회원가입을 한 아이디의 초대코드를 가져오는 메소드
+        private fun getInviteCode() {
+            // singleUserColumn id 를 preference 에서 추출해 서버로 전송
+            val postJsonObject = JSONObject()
+            val userColumnID = SharedPreferenceHelper.getInt(this@SignUpSecondActivity, getText(R.string.userColumnID).toString())
+            postJsonObject.put("what", "getInvitationCode")
+            postJsonObject.put("singleUserID", userColumnID)
+
+            httpConnectionService.serverPostRequest(serverPage, postJsonObject.toString(), REQUEST_SIMPLE_POST_METHOD, GET_INVITE_CODE)
+            Log.i(TAG, "Request : 초대코드 서버로부터 받아오는 요청 보냄")
+        }
+
+        // 회원가입이 모두 완료됬을 때 호출하는 메소드
+        private fun onSignUpSuccess(activity: AppCompatActivity, coupleColumnID: Int) {
             val toHome = Intent(activity, MainHomeActivity::class.java)
             val coupleColumnKey = activity.getText(R.string.coupleColumnID).toString()
             val partnerConnection = activity.getText(R.string.partnerConnection).toString()
 
             // 커플 컬럼 아이디를 shared 에 저장
-            SharedPreferenceHelper.setInt(activity, coupleColumnKey, parseInt(msg.obj.toString()))
+            SharedPreferenceHelper.setInt(activity, coupleColumnKey, coupleColumnID)
             SharedPreferenceHelper.setBoolean(activity, partnerConnection, true)
+
+            // 처음 홈에 들어가게 되면 프로필 설정 액티비티로 전환하기 위한 boolean 값을 같이 넘겨준다
             toHome.putExtra("firstTime", true)
             activity.startActivity(toHome)
         }
+
+        // 실패했을 때 다이얼로그 보여주는 메소드
+        private fun failedDialog(message: String) {
+            val alertDialog = InformDialog()
+            val arguments = Bundle()
+
+            arguments.putString("title", "알림창")
+            arguments.putString("message", message)
+
+            alertDialog.arguments = arguments
+            alertDialog.show(this@SignUpSecondActivity.supportFragmentManager, "FailedDialog")
+        }
     }
 }
-
