216aa9d7be1cb34d94e4af23e20d8b221afc76d2
ByungHakNoh <nbh6579@naver.com>
2020-06-15 11:08:58 +0900

    [modify] ItemClickListener : onItemLongClick 으로 메소드명 변경

--- app/src/main/java/org/personal/coupleapp/interfaces/recyclerView/ItemClickListener.kt
+++ app/src/main/java/org/personal/coupleapp/interfaces/recyclerView/ItemClickListener.kt
@@ -4,5 +4,5 @@ import android.view.View
 
 interface ItemClickListener {
     fun onItemClick(view: View?, itemPosition: Int)
-    fun onItemOnClick(view:View?, itemPosition: Int)
+    fun onItemLongClick(view:View?, itemPosition: Int)
 }
