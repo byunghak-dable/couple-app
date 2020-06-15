4007a061f8c0200847ca21c614c6a4c2d2b10892
ByungHakNoh <nbh6579@naver.com>
2020-06-15 09:50:45 +0900

    [modify] 채팅 어뎁터 채팅 보여줄 때 뷰타입으로 내 채팅 분리해서 보여주기

--- app/src/main/java/org/personal/coupleapp/adapter/ChatAdapter.kt
+++ app/src/main/java/org/personal/coupleapp/adapter/ChatAdapter.kt
@@ -1,68 +1,73 @@
 package org.personal.coupleapp.adapter
 
 import android.content.Context
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
-import androidx.cardview.widget.CardView
-import androidx.constraintlayout.widget.ConstraintLayout
-import androidx.constraintlayout.widget.ConstraintSet
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import org.personal.coupleapp.R
-import org.personal.coupleapp.data.CoupleChatData
+import org.personal.coupleapp.data.ChatData
 
 
-class ChatAdapter(val context: Context, private val myUserID: Int, private val messageList: ArrayList<CoupleChatData>) :
-    RecyclerView.Adapter<ChatAdapter.ViewHolder>() {
+class ChatAdapter(val context: Context, private val myUserID: Int, private val messageList: ArrayList<ChatData>) :
+    RecyclerView.Adapter<RecyclerView.ViewHolder>() {
 
     private val TAG = javaClass.name
 
-    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        val profileCV:CardView = itemView.findViewById(R.id.profileCV)
+    private val MY_CHAT = 1
+    private val OTHER_CHAT = 2
+
+    class OthersChatViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         val profileImageIV: ImageView = itemView.findViewById(R.id.profileIV)
         val nameTV: TextView = itemView.findViewById(R.id.nameTV)
-        val messageTV: TextView = itemView.findViewById(R.id.messageTV)
-        val itemLayout: ConstraintLayout = itemView.findViewById(R.id.chattingItemCL)
-        val messageTimeTV: TextView = itemView.findViewById(R.id.messageTimeTV)
+        val otherMessageTV: TextView = itemView.findViewById(R.id.messageTV)
+        val otherMessageTimeTV: TextView = itemView.findViewById(R.id.messageTimeTV)
     }
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_chatting, parent, false)
-        return ViewHolder(view)
+    class MyChatViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val myMessageTV: TextView = itemView.findViewById(R.id.messageTV)
+        val myMessageTimeTV: TextView = itemView.findViewById(R.id.messageTimeTV)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        val inflater = LayoutInflater.from(parent.context)
+        val view: View
+
+        if (viewType == MY_CHAT) {
+            view = inflater.inflate(R.layout.item_my_chat, parent, false)
+            return MyChatViewHolder(view)
+        }
+        view = inflater.inflate(R.layout.item_others_chat, parent, false)
+        return OthersChatViewHolder(view)
+    }
+
+    override fun getItemViewType(position: Int): Int {
+        if (myUserID == messageList[position].sender_id) {
+            return MY_CHAT
+        }
+        return OTHER_CHAT
     }
 
     override fun getItemCount(): Int {
         return messageList.size
     }
 
-    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        val chatData: CoupleChatData = messageList[position]
-        val constraintSet = ConstraintSet()
-        constraintSet.clone(holder.itemLayout)
-        Log.i(TAG, chatData.toString())
-        // 채팅 메시지 넣기
-        holder.messageTV.text = chatData.message
-        holder.messageTimeTV.text = chatData.message_time
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        val messageData = messageList[position]
 
-        if (chatData.sender_id == myUserID) {
-            // 레이아웃 변경
-            constraintSet.clear(holder.messageTV.id, ConstraintSet.START)
-            constraintSet.clear(holder.messageTimeTV.id, ConstraintSet.END)
-            constraintSet.connect(holder.messageTV.id, ConstraintSet.END, holder.itemLayout.id, ConstraintSet.END)
-            constraintSet.connect(holder.messageTimeTV.id, ConstraintSet.END, holder.messageTV.id, ConstraintSet.START)
-            constraintSet.applyTo(holder.itemLayout)
-
-        }else {
-            // 프로필 이미지, 이름 넣기
-            holder.profileCV.visibility = View.VISIBLE
-            holder.nameTV.visibility = View.VISIBLE
-
-            Glide.with(context).load(chatData.profile_image).into(holder.profileImageIV)
-            holder.nameTV.text = chatData.name
+        if (myUserID == messageData.sender_id) {
+            val myChatViewHolder = holder as MyChatViewHolder
+            myChatViewHolder.myMessageTV.text  = messageData.message
+            myChatViewHolder.myMessageTimeTV.text = messageData.message_time
+        } else {
+            val othersChatViewHolder = holder as OthersChatViewHolder
+            Glide.with(context).load(messageData.profile_image).into(othersChatViewHolder.profileImageIV)
+            othersChatViewHolder.nameTV.text = messageData.name
+            othersChatViewHolder.otherMessageTV.text = messageData.message
+            othersChatViewHolder.otherMessageTimeTV.text = messageData.message_time
         }
     }
 }
